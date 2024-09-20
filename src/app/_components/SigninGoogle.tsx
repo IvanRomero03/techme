1259253cc5e8fb9fbeb0f876f63b6a5390660b11
Iván Romero <i.wells.ar@gmail.com>
2024-09-20 17:48:59 -0600

    Update src/app/_components/SigninGoogle.tsx
    Co-authored-by: Esteban Aleman M. <161876946+EstebanAleman21@users.noreply.github.com>

--- src/app/_components/SigninGoogle.tsx
+++ src/app/_components/SigninGoogle.tsx
@@ -7,7 +7,7 @@ export default function SigninGoogle() {
   return (
     <button
       className="flex w-full items-center justify-center rounded-md border border-zinc-300 bg-white py-1 text-zinc-700"
-      onClick={() => signIn("google", { callbackUrl: "/login/redirect" })}
+      onClick={() => signIn("google", { callbackUrl: "/login/redirects" })}
     >
       <FaGoogle className="mr-2 text-red-700" />
       Sign in with Google
