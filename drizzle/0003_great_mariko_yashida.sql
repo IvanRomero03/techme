e70966d15faf843c0fba1fc63cc62ea0b5944111
Esteban <esteban21112002@gmail.com>
2024-11-11 16:53:03 -0600

    Gitignore

--- drizzle/0003_great_mariko_yashida.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "techme_project_tasks" ADD COLUMN "assigned_to" varchar(255);--> statement-breakpoint
-ALTER TABLE "techme_project_tasks" ADD COLUMN "due_date" timestamp with time zone;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "techme_project_tasks" ADD CONSTRAINT "techme_project_tasks_assigned_to_techme_user_id_fk" FOREIGN KEY ("assigned_to") REFERENCES "public"."techme_user"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "task_assigned_to_idx" ON "techme_project_tasks" USING btree ("assigned_to");
--- drizzle/0004_remarkable_norrin_radd.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "techme_project_tasks" DROP CONSTRAINT "techme_project_tasks_assigned_to_techme_user_id_fk";
---> statement-breakpoint
-DROP INDEX IF EXISTS "task_assigned_to_idx";--> statement-breakpoint
-ALTER TABLE "techme_project_tasks" DROP COLUMN IF EXISTS "assigned_to";--> statement-breakpoint
-ALTER TABLE "techme_project_tasks" DROP COLUMN IF EXISTS "due_date";
