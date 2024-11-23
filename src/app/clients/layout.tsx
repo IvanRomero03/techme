5f13f22adb561110fd3ab9932bf87d69c805f998
Ivan Romero <i.wells.ar@gmail.com>
2024-11-22 18:31:22 -0600

    roleprotect

---
+++ src/app/clients/layout.tsx
@@ -0,0 +1,22 @@
+import { type Metadata } from "next";
+import { getServerAuthSession } from "techme/server/auth";
+import { redirect } from "next/navigation";
+import { UserRole } from "techme/util/UserRole";
+
+export const metadata: Metadata = {
+  title: "TechMe Projects",
+  description: "Neoris & TechMe",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default async function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const session = await getServerAuthSession();
+  if (!session?.user || session.user.role === UserRole.Unauthorized) {
+    redirect("/");
+  }
+  return children;
+}
