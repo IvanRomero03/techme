598f902ed71add04f2dcd0df6c5b277e1e8c403d
Ivan Romero <i.wells.ar@gmail.com>
2024-11-25 16:22:50 -0600

    aaaa

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
 // app/dashboard/admin.tsx
-import React from "react";
+import { redirect } from "next/navigation";
+import { getServerAuthSession } from "techme/server/auth";
+import { UserRole } from "techme/util/UserRole";
 
-const AdminPage: React.FC = () => {
+const AdminPage = async () => {
+  const session = await getServerAuthSession();
+  if (!session?.user || session.user.role === UserRole.Unauthorized) {
+    redirect("/");
+  }
+  const desiredUrl = "/dashboard/" + session.user.role.toLowerCase();
+  redirect(desiredUrl);
   return <h1>Please Login with a Registered Account with valid role</h1>;
 };
 
